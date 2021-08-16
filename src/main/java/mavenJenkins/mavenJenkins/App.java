39b3758e76ebe98a08a79948013e39c44c6e7ec9
christianzunalargo2 <christian.zunalargo@outlook.com>
2021-08-16 11:47:47 -0400

    first commit

---
+++ src/main/java/mavenJenkins/mavenJenkins/App.java
@@ -0,0 +1,13 @@
+package mavenJenkins.mavenJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
