8182ab28634346893eac137988f3cce174b20b02
christianzunalargo2 <christian.zunalargo@outlook.com>
2021-08-16 12:22:47 -0400

    second commit

---
+++ src/test/java/steps/GoogleSteps.java
@@ -0,0 +1,22 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GoogleSteps {
+	
+	@Given("user opens browser")
+	public void user_opens_browser() {
+	}
+
+	@When("user hits enter")
+	public void user_hits_enter() {
+	}
+
+	@Then("results are shown")
+	public void results_are_shown() {
+	}
+
+
+}
---
+++ src/test/resources/Features/cucumber.feature
@@ -0,0 +1,7 @@
+Feature: testing cucumber with google search
+
+Scenario: testing search box
+Given user opens browser
+When user hits enter
+Then results are shown
+
